071c78fbaeb49e3cf9bd98c7dfe5b18410411613
MouRen-Luo <2382965630@qq.com>
2020-02-29 11:31:54 +0800

    init repo

---
+++ src/test/java/com/lsg/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsg.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
