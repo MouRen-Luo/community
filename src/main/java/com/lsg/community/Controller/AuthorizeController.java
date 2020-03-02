09d9ed5d10d85d6360b6680a71a27cbf1215024a
MouRen-Luo <2382965630@qq.com>
2020-03-02 11:58:18 +0800

    properties

--- src/main/java/com/lsg/community/Controller/AuthorizeController.java
+++ src/main/java/com/lsg/community/Controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.lsg.community.Dto.AccessTokenDTO;
 import com.lsg.community.Dto.GithubUser;
 import com.lsg.community.Provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,14 +15,21 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
                            @RequestParam(name = "state")String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("9649e173e7bd06cf074b");
-        accessTokenDTO.setClient_secret("ff64fae4c0fcb2248e0b04b809da8df4f794f63f");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
