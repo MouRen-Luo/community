2fa32bcf829ddbf4dc29425c9053a9df77786836
MouRen-Luo <2382965630@qq.com>
2020-03-02 16:16:58 +0800

    2020-3-2 edit

--- src/main/java/com/lsg/community/Controller/AuthorizeController.java
+++ src/main/java/com/lsg/community/Controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.lsg.community.Controller;
 
 import com.lsg.community.Dto.AccessTokenDTO;
 import com.lsg.community.Dto.GithubUser;
+import com.lsg.community.Mapper.UserMapper;
+import com.lsg.community.Model.User;
 import com.lsg.community.Provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,11 +11,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
 @Controller
 public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;
+    @Autowired
+    private UserMapper userMapper;
 
     @Value("${github.client.id}")
     private String clientId;
@@ -22,9 +31,17 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    /**
+     * github登录
+     * @param code
+     * @param state
+     * @return
+     */
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
-                           @RequestParam(name = "state")String state){
+                           @RequestParam(name = "state")String state,
+                           HttpServletRequest request,
+                           HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -32,8 +49,22 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if(githubUser!=null){
+            //登录成功
+            User user = new User();
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);//保存进数据库
+            response.addCookie(new Cookie("token",token));
+            return "redirect:/";
+        }else {
+            return "redirect:/";
+            //登录失败
+        }
     }
 }
---
+++ src/main/java/com/lsg/community/Mapper/UserMapper.java
@@ -0,0 +1,20 @@
+package com.lsg.community.Mapper;
+
+import com.lsg.community.Model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+
+    /**
+     * 新增用户
+     * @param user
+     */
+    @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+
+    @Select("select * from user where token=#{token}")
+    User findByToken(String token);
+}
